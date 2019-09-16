ca499fe6ce87cac6b07d76d822e7feb4d7d8dea4
Aaron Stannard <aaron@petabridge.com>
2019-09-16 16:15:14 -0500

    added complete example

---
+++ src/Demo/JsonSerializerWithManifest.cs
@@ -0,0 +1,35 @@
+﻿using Akka.Actor;
+using Akka.Configuration;
+using Akka.Serialization;
+
+namespace Demo
+{
+    public class JsonSerializerWithManifest : NewtonSoftJsonSerializer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NewtonSoftJsonSerializer" /> class.
+        /// </summary>
+        /// <param name="system">The actor system to associate with this serializer. </param>
+        public JsonSerializerWithManifest(ExtendedActorSystem system)
+            : base(system, NewtonSoftJsonSerializerSettings.Default)
+        {
+        }
+
+        public JsonSerializerWithManifest(ExtendedActorSystem system, Config config)
+            : base(system, NewtonSoftJsonSerializerSettings.Create(config))
+        {
+        }
+
+
+        public JsonSerializerWithManifest(ExtendedActorSystem system, NewtonSoftJsonSerializerSettings settings)
+            : base(system)
+        {
+        }
+
+        /// <summary>
+        ///  Always include manifest, so https://github.com/akkadotnet/akka.net/blob/36a7267393c86c14380d7c17205c866e58f3123c/src/contrib/persistence/Akka.Persistence.Sql.Common/Journal/QueryExecutor.cs#L695-L710
+        ///  invokes manifest write to SQL.
+        /// </summary>
+        public override bool IncludeManifest => true;
+    }
+}
---
+++ src/Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using Akka.Actor;
+using Akka.Configuration;
+using System;
+using System.IO;
+
+namespace Demo
+{
+    public class MyJsonMsg
+    {
+        public MyJsonMsg(string foo, string bar)
+        {
+            Foo = foo;
+            Bar = bar;
+        }
+
+        public string Foo { get; }
+
+        public string Bar { get; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = File.ReadAllText("reference.conf");
+            var actorSystem = ActorSystem.Create("JsonWithManifestDemo", 
+                ConfigurationFactory.ParseString(config));
+
+            var serializer = actorSystem.Serialization.FindSerializerForType(typeof(MyJsonMsg));
+            actorSystem.Log.Info("Found serializer of type [{0}, Id={1}] for msg of type {2}", serializer, serializer.Identifier, typeof(MyJsonMsg));
+
+            Console.ReadLine();
+        }
+    }
+}
